2989675abc4bdc50dfa4be406945daa3a543d3dc
Michelle <michelle-heintze@web.de>
2023-03-06 20:25:46 +0100

    test

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'mhtodolist',
+    appId: '1:720450097284:web:6c71c4f41bdbca3e7f32d1',
+    storageBucket: 'mhtodolist.appspot.com',
+    apiKey: 'AIzaSyBq6UeWsW9xSHifwZiWRm7B60Fdeu5sx_o',
+    authDomain: 'mhtodolist.firebaseapp.com',
+    messagingSenderId: '720450097284',
+    measurementId: 'G-BNGKMX22PG',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'mhtodolist',
+    appId: '1:720450097284:web:6c71c4f41bdbca3e7f32d1',
+    storageBucket: 'mhtodolist.appspot.com',
+    apiKey: 'AIzaSyBq6UeWsW9xSHifwZiWRm7B60Fdeu5sx_o',
+    authDomain: 'mhtodolist.firebaseapp.com',
+    messagingSenderId: '720450097284',
+    measurementId: 'G-BNGKMX22PG',
+  },
   production: false
 };
 
